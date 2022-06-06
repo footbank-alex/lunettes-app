864f1e8ad9eeb619330cd491445a282b65b1baaa
Alexander Bittermann <alexander.bittermann@gmx.at>
2022-06-06 22:21:15 +0900

    fixed mock config

--- mock-data.ts
+++ mock-data.ts
@@ -1,8 +1,8 @@
-import {Endpoints} from "./src/api/endpoint";
+import {Endpoint} from "./src/api/endpoint";
 import {DateTime} from "luxon";
 
-export const getEndpointsResponse: Endpoints.Endpoint[] = Array.from(Array(30).keys()).map(value => ({
-    id: value.toString(),
-    itemName: `Test ${value + 1}`,
-    dateTime: DateTime.now().plus({days: value}).toJSON()
-}));
+export const getEndpointsResponse: Endpoint[] = Array.from(Array(30).keys()).map(value => new Endpoint(
+    value.toString(),
+    `Test ${value + 1}`,
+    DateTime.now().plus({days: value})
+));
